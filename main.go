de92e3e16d08ca0a0fc05a510ee4d0712ddc7a87
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-06-29 21:13:17 -0400

    added comments to clarify how fillSumTree works

--- main.go
+++ main.go
@@ -9,53 +9,53 @@ import (
 	"strings"
 )
 
-type templateData struct {
-	Input, Output       []int
-}
-
 // make array-based binary tree for sums of subsets of input
-func fillSumTree (input, output []int, n, i int) {
-	// i = input index, n = output index
-	if( i == len(input)){
-		return
-	}
+	// input: array of integers from which subsets will be summed
+	// binTree: breadth-first representation of binary tree of sums
+	// nodeIndex: index of binTree
+	// inputIndex: index of input 
+func fillSumTree (input, binTree []int, nodeIndex, inputIndex int) {
+	// return when at end of input
+	if( inputIndex == len(input)){ return }
 
-	output[2*n+1] = output[n]+input[i] // left node
-	output[2*n+2] = output[n] // right node
+	binTree[2*nodeIndex+1] = binTree[nodeIndex]+input[inputIndex] // left node = current node + current input
+	binTree[2*nodeIndex+2] = binTree[nodeIndex] // right node = current node
 
-	// i+1 for each call to match depth
-	fillSumTree(input,output,2*n+1,i+1) // descend left 
-	fillSumTree(input,output,2*n+2,i+1) // descend right
+	// inputIndex+1 for each call to maintain uniform depth on descend
+	fillSumTree(input,binTree,2*nodeIndex+1,inputIndex+1) // descend left 
+	fillSumTree(input,binTree,2*nodeIndex+2,inputIndex+1) // descend right
 }
 
+// handle form input
 func handler(w http.ResponseWriter, r *http.Request) {
+	// get form input "body", which will be a string of space-separated numbers
 	inputArr := strings.Split(r.FormValue("body"), " ")
-	nums := []int{}
+
+	nums := []int{} // array of input to send to fillSumTree
+	// convert strings in inputArr to integers and append to nums
 	for _, v := range inputArr {
 		n, _ := strconv.Atoi(v)
 		nums = append(nums, n)
 	}
 
+	// binary tree array to hold sums of all subets.
+	// Length will accomodate all nodes for breadth-first representation
+		// |bin. sum tree| = 2**(|input|+1) - 1
+			// e.g. |input| = 2
+			// => |len of bin. tree| = (2**3)-1 = 7
+	binTree := make([]int, 1<<(len(nums)+1)-1)
+
+	fillSumTree(nums,binTree,0,0)
+
+	binTreeString := []string{}
+	for _,v := range binTree {
+		binTreeString = append(binTreeString,strconv.Itoa(v))
+	}
+
 	t, _ := template.ParseFiles("index.html")
+	exec.Command("./graph-plotter.py", binTreeString...).Run()
 
-	output := make([]int, 1<<(len(nums)+1)-1) // accomodate all nodes for breadth-first representation
-
-	fillSumTree(nums,output,0,0)
-
-	outputString := []string{}
-	for _,v := range output {
-		outputString = append(outputString,strconv.Itoa(v))
-	}
-
-	//  err := exec.Command("./graph-plotter.py", outputString...).Run()
-	exec.Command("./graph-plotter.py", outputString...).Run()
-	/*
-	if err != nil {
-		fmt.Println(err)
-	}
-	*/
-
-	t.Execute(w, struct { Input, Output []int }{nums, output})
+	t.Execute(w, struct { Input, Output []int }{nums, binTree})
 }
 
 func main() {
