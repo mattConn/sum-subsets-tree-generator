b9e22303d5500017e6d507ba86bc5887d338a88f
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-06-30 00:28:06 -0400

    clarifying code in graph plotting script

--- graph-plotter.py
+++ graph-plotter.py
@@ -9,42 +9,40 @@ import matplotlib.pyplot as plt
 import pygraphviz
 from networkx.drawing.nx_agraph import graphviz_layout
 
-# graph drawing options
+# graph drawing
 def draw(graph):
     pos=graphviz_layout(graph, prog='dot')
     nx.draw_networkx(graph,pos,font_color='black',node_color='orange',font_size=15,node_size=700,)
-    plt.title("depth:node:sum")
+    plt.title("node:sum")
 
-sys.argv[1:] = [int(i) for i in sys.argv[1:]]
+# cast argv (binary tree data) to int
+treeData = [int(i) for i in sys.argv[1:]]
 
-depth = int(math.log(len(sys.argv[1:])+1,2))
+# dimensions of tree
+depth = int(math.log(len(treeData)+1,2))
 maxNodesAtDepth = 2**(depth-1)
 
-# plotting options
+# size plot by tree dimensions
 plt.figure(figsize=(maxNodesAtDepth,depth))
 
 # construct graph
 g = nx.DiGraph();
 
-#for i in sys.argv[1:]:
-#    g.add_node(i)
+# traverse tree data array and add edges to networkx graph
+for dataIndex in range(len(treeData)):
+    leftIndex = 2*dataIndex+1 # index of left node in tree
+    rightIndex = 2*dataIndex+2 # index of right node in tree
 
-for dataIndex in range(len(sys.argv[1:])):
-    leftIndex = 2*dataIndex+1
-    rightIndex = 2*dataIndex+2
+    if leftIndex >= len(treeData): break # break if end of data
 
-    if leftIndex >= len(sys.argv[1:]): break 
-
-    def getDepth(index): return int(math.log(index+1,2))
-
-    # for connecting current node to another
+    # for connecting current node to another node.
     # labelling each node as "dataIndex:data[dataIndex]"
-    def joinCurNodeTo(otherIndex):
-        g.add_edge(f'{getDepth(dataIndex)}:{dataIndex}:{sys.argv[1:][dataIndex]}', f'{getDepth(otherIndex)}:{otherIndex}:{sys.argv[1:][otherIndex]}')
+    def joinCurrentNodeTo(otherIndex):
+        g.add_edge(f'{dataIndex}:{treeData[dataIndex]}', f'{otherIndex}:{treeData[otherIndex]}')
 
-    joinCurNodeTo(leftIndex)
-    joinCurNodeTo(rightIndex)
+    joinCurrentNodeTo(leftIndex)
+    joinCurrentNodeTo(rightIndex)
     
 
-draw(g)
-plt.savefig("assets/graph.png")
+draw(g) # draw graph
+plt.savefig("assets/graph.png") # save graph image
